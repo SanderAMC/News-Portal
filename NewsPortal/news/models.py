402d636e82ddb4c4516181e5d61e8bc6c43b01e9
SanderAMC <sanderamc@gmail.com>
2023-01-06 08:36:16 +0300

    fill bases 1

--- NewsPortal/news/models.py
+++ NewsPortal/news/models.py
@@ -5,37 +5,51 @@ from django.contrib.auth.models import User
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    rating = models.IntegerField()
+    rating = models.IntegerField(default=0)
 
     def update_rating(self):
+        """
+        суммарный рейтинг каждой статьи автора умножается на 3;
+        суммарный рейтинг всех комментариев автора;
+        суммарный рейтинг всех комментариев к статьям автора.
+
+        """
+        posts = Post.objects.filter(Post.author == self.user).values("rating")
         pass
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    pass
 
 
 class Post(models.Model):
+    TYPES = [
+        ('A', 'Article'),
+        ('N', 'News')
+    ]
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    post_type = models.BooleanField()
+    post_type = models.CharField(max_length=1, choices=TYPES, default='A')
     creation = models.DateTimeField(auto_now_add=True)
     category = models.ManyToManyField(Category, through='PostCategory')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    rating = models.IntegerField()
+    rating = models.IntegerField(default=0)
 
 
     def like(self):
-        pass
+
+        self.rating += 1
+        self.save()
 
 
     def dislike(self):
-        pass
 
+        if self.rating > 0:
+            self.rating -= 1
+        self.save()
 
     def preview(self):
-        pass
+        return self.text[:125] + ' ...'
 
 
 class PostCategory(models.Model):
@@ -49,11 +63,13 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     creation = models.DateTimeField(auto_now_add = True)
-    rating = models.IntegerField()
+    rating = models.IntegerField(default=0)
+
 
     def like(self):
-        pass
+        self.rating += 1
 
 
     def dislike(self):
-        pass
+        if self.rating > 0:
+            self.rating -= 1
