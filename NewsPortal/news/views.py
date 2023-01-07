c816e0711e70b157e9fc14acc7bfddc5704fb98d
SanderAMC <sanderamc@gmail.com>
2023-01-07 19:39:22 +0300

    stage 3, filters, pagination

--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -2,7 +2,8 @@
 # что в этом представлении мы будем выводить список объектов из БД
 from django.views.generic import ListView, DetailView
 from .models import Post
-from datetime import datetime
+from .filters import NewsFilter
+
 
 class NewsList(ListView):
     # Указываем модель, объекты которой мы будем выводить
@@ -15,11 +16,28 @@ class NewsList(ListView):
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'news_list'
+    paginate_by = 2
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['news_count'] = Post.objects.all().count
+        # Добавляем в контекст объект фильтрации.
+        context['filterset'] = self.filterset
         return context
+
+   # Переопределяем функцию получения списка новостей
+    def get_queryset(self):
+       # Получаем обычный запрос
+       queryset = super().get_queryset()
+       # Используем наш класс фильтрации.
+       # self.request.GET содержит объект QueryDict, который мы рассматривали
+       # в этом юните ранее.
+       # Сохраняем нашу фильтрацию в объекте класса,
+       # чтобы потом добавить в контекст и использовать в шаблоне.
+       self.filterset = NewsFilter(self.request.GET, queryset)
+       # Возвращаем из функции отфильтрованный список товаров
+       return self.filterset.qs
+
 
 class OneNews(DetailView):
     # Указываем модель, объекты которой мы будем выводить
